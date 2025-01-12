cf1fec5cfd16073f8412c6b975ad497724cc49e0
Nam Nguyen <nguy1053@msu.edu>
2025-01-12 14:31:10 -0500

    Setup grpc server basic and order service

---
+++ services/orders/service/orders.go
@@ -0,0 +1,9 @@
+package service /// online business logic, apart from transport or grpc layer
+
+type OrderService struct {
+	
+}
+
+func NewOrderService() *OrderService {
+	return &OrderService{}
+}
